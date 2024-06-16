7b7186de96ec50daaae779e68e69889d0143b983
Luthfii1 <luthfi.misbachul@ui.ac.id>
2024-06-17 00:18:18 +0700

    first commit

---
+++ CandyPotion/CandyPotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandyPotionApp.swift
+//  CandyPotion
+//
+//  Created by Luthfi Misbachul Munir on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CandyPotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ CandyPotion/View/Pages/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  CandyPotion
+//
+//  Created by Luthfi Misbachul Munir on 17/06/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, this is main page!")
+    }
+}
+
+#Preview {
+    MainView()
+}
